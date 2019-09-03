deb9280ec74ea5c90a395efdc4f61eb11e525949
GSCrawley <gscrawley@gmail.com>
2019-09-03 14:25:28 -0700

    this better be the last time

---
+++ madlibs.py
@@ -0,0 +1,62 @@
+# print("Enter a color ")
+# color = [input()]
+# print("enter a plural noun ")
+# plural_noun = [input()]
+# print("enter a celebrity's name")
+# celebrity = [input()]
+#
+# def show():
+# print("roses are "+color)
+# print(plural_noun + "are blue")
+# print("I love " + celebrity)
+#
+# show()
+
+name = input("enter a name"))
+body_part = input("enter a body part")
+adj = input("enter an adjective")
+noun = input("enter a noun")
+adverb = input("enter an adverb")
+exclamation = input("enter an exclamation")
+past_tense_verb = input("enter a verb in the past tense")
+body_part1 = input("enter a body part")
+past_tense_verb1 = input("enter a verb in the past tense")
+noun1 = input("enter a noun")
+adj1 = input("enter an adjective")
+noun2 = input("enter a noun")
+noun3 = input("enter a noun")
+verb = input("enter a verb")
+adj2 = input("enter an adjective")
+adj3 = input("enter an adjective")
+adj4 = input("enter an adjective")
+adj5 = input("enter an adjective")
+noun4 = input("enter a noun")
+noun5 = input("enter a noun")
+body_part2 = input("enter a body part")
+body_part3 = input("enter a body part")
+body_part4 = input("enter a body part")
+past_tense_verb2 = input("enter a verb in the past tense")
+noun6 = input("enter a noun")
+noun7 = input("enter a noun")
+
+def show():
+    print("FUN IN A MUD SLIDE")
+    print("Whoosh! Troop Leader " + name + "went sliding. He landed " + body_part + "-first in "+ adj +"mud.")
+    print("'It’s okay,' he called back to the "+noun+". I meant to do that. I was just trying to show you how much fun
+    it is to go " + adverb+". ”)
+    print(exclamation +  " ! George" + past_tense_verb + ". He lay on his "+body_part1+" and
+    down in the mud "+body_part2+"-first.")
+    print(noun1+”! Chris shouted. 'It’s" + adj1+"-man to the rescue!' He got on his "+noun2+
+    "and " + body_part3+"-first down the "+noun3+" and into the "+noun4+".")
+    print("“Hey, George!” Alex cried out."+verb+“ fast!”)
+    print("George grinned as Alex threw a  pile of " +noun5+ "right at him. There was " +noun5+" in his "
+    +body_part3+ ", in his " +body_part4+ ", and under his " +noun6+". Some mud had even " + past_tense_verb2 +" its way into his " + noun7+ " and onto
+    his " +body_part1+".")
+
+show()
+
+# # This was really fun!
+# # Reproducible activity from
+# # George Brown, Class Clown: Trouble Magnet
+# #  by Nancy Krulik
+# # Reproducible activity from

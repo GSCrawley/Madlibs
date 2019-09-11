25f88c1f15e6485fbea548e48e4081ffb6648ba1
Gideon S Crawley <gscrawley@gmail.com>
2019-09-11 11:59:11 -0700

    Add files via upload
    I had this is the incorrect repository, so am adding it manually now. I  did 6 commits while writing the file

--- madlibs.py
+++ madlibs.py
@@ -1,43 +1,25 @@
-name = input("enter a name")
-body_part = input("enter a body part")
-adj = input("enter an adjective")
-noun = input("enter a noun")
+name = input("enter a name: ")
+a,b,c,d,e,f = input("enter 6 different body parts: ").split()
+g,h = input("enter 2 different adjectives").split()
+i,j,k,l,m,n,o,p = input("enter 8 different nouns").split()
 adverb = input("enter an adverb")
 exclamation = input("enter an exclamation")
-past_tense_verb = input("enter a verb in the past tense")
-body_part1 = input("enter a body part")
-past_tense_verb1 = input("enter a verb in the past tense")
-noun1 = input("enter a noun")
-adj1 = input("enter an adjective")
-noun2 = input("enter a noun")
-noun3 = input("enter a noun")
+q,r = input("enter 2 different verbs in the past tense").split()
 verb = input("enter a verb")
-adj2 = input("enter an adjective")
-adj3 = input("enter an adjective")
-adj4 = input("enter an adjective")
-adj5 = input("enter an adjective")
-noun4 = input("enter a noun")
-noun5 = input("enter a noun")
-body_part2 = input("enter a body part")
-body_part3 = input("enter a body part")
-body_part4 = input("enter a body part")
-past_tense_verb2 = input("enter a verb in the past tense")
-noun6 = input("enter a noun")
-noun7 = input("enter a noun")
 
 def show():
     return f'''FUN IN A MUD SLIDE
-    Whoosh! Troop Leader {name} went sliding. He landed { body_part } -first in { adj } mud.
-    "It’s okay", he called back to the { noun }. I meant to do that. I was just trying to show you how much fun
+    Whoosh! Troop Leader {name} went sliding. He landed { a } -first in { g } mud.
+    "It’s okay", he called back to the { i }. I meant to do that. I was just trying to show you how much fun
     it is to go { adverb }.
-    {exclamation} ! George { past_tense_verb }. He lay on his {body_part1} and
-    down in the mud {body_part2}-first.
-    {noun1}! Chris shouted It’s {adj1}-man to the rescue! He got on his {noun}
-    and {body_part3}-first down the {noun3} and into the {noun4}.)
+    {exclamation} ! George { q }. He lay on his { b } and
+    down in the mud { c }-first.
+    { p }! Chris shouted It’s { h }-man to the rescue! He got on his { j }
+    and { d }-first down the { k } and into the { l }.)
     Hey, George! Alex cried out. {verb} fast!
-    George grinned as Alex threw a pile of {noun5} right at him. There was {noun5} in his
-    {body_part3}, in his {body_part4}, and under his {noun6}. Some mud had even {past_tense_verb2} its way into his {noun7} and onto
-    his {body_part1}.'''
+    George grinned as Alex threw a pile of { m } right at him. There was { m } in his
+    { e }, in his { f }, and under his { n }. Some { m } had even { r } its way into his { o } and onto
+    his { a }.'''
 
 print(show())
 
